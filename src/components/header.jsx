f24dca058ade3085cb24b65e3303e8e5ae33f0a5
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-02-13 13:10:46 +0200

    implemented initial responsive styles to the navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,14 +10,8 @@ function Header() {
   return (
     <header className="flex items-center bg-gray-900 py-6 px-3.5 relative ">
       <h1 className="text-4xl text-slate-100 font-bold">MHEN</h1>
-      <div className="hover:cursor-pointer" onClick={handleClick}>
-        <FontAwesomeIcon
-          icon={click ? faX : faBars}
-          size="2xl"
-          style={{ color: "#f3f1f8" }}
-        />
-      </div>
-      <nav className="m-auto">
+
+      <nav className="m-auto hidden md:block">
         <ul className="flex gap-7 text-slate-100 font-bold">
           <li>Home</li>
           <Dropdown
@@ -48,6 +42,16 @@ function Header() {
           <li>Contact us</li>
         </ul>
       </nav>
+      <div
+        className="hover:cursor-pointer md:hidden ml-auto"
+        onClick={handleClick}
+      >
+        <FontAwesomeIcon
+          icon={click ? faX : faBars}
+          size="2xl"
+          style={{ color: "#f3f1f8" }}
+        />
+      </div>
     </header>
   );
 }
