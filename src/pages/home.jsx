79c5f624f4c7f513529b18cf079b851354be9d88
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-02-19 12:54:12 +0200

    modified Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,11 @@
 function Home() {
   return (
-    <div className="bg-gray-500 h-80">
-      <div className="flex flex-col justify-center items-center h-full px-4">
-        <h3 className="text-3xl text-white font-bold">
-          Malawi Health Equity Network
-        </h3>
-        <p className="text-white">
-          Promoting Equitable access to quality,affordable and responsive health
-          care services in Malawi
+    <div className="bg-gray-500 h-96">
+      <div className="flex flex-col justify-center h-full px-4 text-white">
+        <h3 className="text-3xl font-bold">Malawi Health Equity Network</h3>
+        <p className="mt-4 font-bold">
+          Promoting Equitable access to quality, affordable and responsive
+          health care services in Malawi
         </p>
       </div>
     </div>
