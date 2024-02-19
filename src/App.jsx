fcebf5a096dfc45dd33b8534510d71f85ca223ec
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-02-19 13:28:23 +0200

    modified file path for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Layout from "./components/Layout";
 
 function App() {
@@ -15,7 +15,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <Header /> */
-}
